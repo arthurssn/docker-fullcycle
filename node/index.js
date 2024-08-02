47d76da9cb5cbea0d11362a0c419c9640e7a820a
Arthur Novaes <arthurnovaes55@gmail.com>
2024-08-02 11:26:23 -0300

    feat: docker compose com node e mysql

--- node/index.js
+++ node/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-
 const app = express();
-
 const port = 3000;
+
+const configDB = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const mysql = require("mysql");
+const connection = mysql.createConnection(configDB);
+
+const createTable = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment primary key, name varchar(255))`;
+connection.query(createTable);
+const insert = `INSERT INTO people(name) values('Arthur')`;
+connection.query(insert);
+connection.end();
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
